621940e6b9bcdce1aab16a761ba0723a2dc39f42
Ornnicha1997 <eyejung6927@hotmail.com>
2019-09-23 08:09:30 +0700

    editAppjs

--- src/App.js
+++ src/App.js
@@ -1,125 +1,50 @@
 import React from 'react';
-import CharacterCard from './CharacterCard';
 import './App.css';
 import _ from 'lodash';
+import CharacterCard from './CharacterCard';
+
  
-
 let message = 'Hello'
-
-
+ 
 const prepareStateFromWord = (given_word) => {
-
   let word = given_word.toUpperCase()
-
   let chars = _.shuffle(Array.from(word))
-
   return {
-
     word,
-
     chars,
-
     attempt: 1,
-
     guess: [],
-
     completed: false,
-
-    check: []
-
+    reset: 1
   }
-
 }
-
-
+ 
 class App extends React.Component {
 
-
   state = prepareStateFromWord(message);
-
-
+  reset =()=>{
+    this.setState({
+      reset: this.state.reset + 1,
+      completed: !this.state.completed
+    })
+  }
   activationHandler = (c) => {
-
     let guess = [...this.state.guess, c]
-    this.setState({ check: [...this.state.check, c] })
     this.setState({ guess })
-
     if (guess.length == this.state.chars.length) {
-
       if (guess.join('').toString() == this.state.word) {
-
         this.setState({ guess: [], completed: true })
-
       } else {
-
         this.setState({ guess: [], attempt: this.state.attempt + 1 })
-
       }
-
     }
-
   }
-
-
+ 
   render() {
-    console.log("chars: ", this.state.chars)
-    console.log("guess: ", this.state.check.join(''))
-    console.log("word: ", this.state.word)
     return (
-
       <div>
-
         {
+          
 
-          Array.from(this.state.chars).map((item, index) => (
-
-            <CharacterCard
-
-              value={item}
-
-              key={index}
-
-              activationHandler={this.activationHandler}
-
-            />
-
-          ))
-
-        }
-
-        <h2>Selected</h2>
-
-        {
-
-          Array.from(this.state.guess).map((item, index) => (
-
-            <CharacterCard
-
-              value={item}
-
-              key={index}
-
-              activationHandler={this.activationHandler}
-
-            />
-
-          ))
-
-        }
-
-        <div>Attemp {this.state.attempt}</div>
-
-        {
-
-          this.state.word === this.state.check.join('') ? <h4>Complete</h4> : ""
-
-        }
-
-      </div>
-
-    )
-
-  }
-
-}
-export default App;
+          
+export default App
