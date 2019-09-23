3362e7de2296aa8602980b48d0ca6ef6a02b074d
Ornnicha1997 <eyejung6927@hotmail.com>
2019-09-23 08:05:49 +0700

    editCharacterCard02

--- src/App.js
+++ src/App.js
@@ -122,6 +122,4 @@ class App extends React.Component {
   }
 
 }
-
- 
 export default App;
--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -14,3 +14,24 @@ class CharacterCard extends React.Component {
         if (this.state.active === false)
         this.props.activationHandler(this.props.value);
     }
+    componentDidUpdate = (prevProps) => {
+        console.log(this.props.attempt);
+        if (prevProps.attempt !== this.props.attempt || prevProps.reset !==this.props.reset) {
+            this.setState({ active: false })
+            console.log('...');
+        }
+    }
+
+    render() {
+        let activeClass = this.state.active ? 'activeCard' : '';
+        let className = `card ${activeClass}`
+        return (
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
+            </div>
+        )
+    }
+}
+ 
+export default CharacterCard;
+
